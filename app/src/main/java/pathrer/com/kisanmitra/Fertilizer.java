d3b144e7df228147b4046a16fdb2a719e78f48df
Niranjan P B <niranjanpb03@gmail.com>
2017-01-24 19:46:29 +0530

    Fertilizer retrival

--- app/src/main/java/pathrer/com/kisanmitra/Fertilizer.java
+++ app/src/main/java/pathrer/com/kisanmitra/Fertilizer.java
@@ -5,17 +5,32 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Fertilizer extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private DatabaseReference mdatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fertilizer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mdatabase = FirebaseDatabase.getInstance().getReference().child("Blog");
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycle);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,4 +41,43 @@ public class Fertilizer extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<Blog , BlogViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Blog, BlogViewHolder>(
+                Blog.class,
+                R.layout.row,
+                BlogViewHolder.class,
+                mdatabase
+        ) {
+
+            @Override
+            protected void populateViewHolder(BlogViewHolder viewHolder, Blog model, int position) {
+                viewHolder.setTitle(model.getTitle());
+                viewHolder.setDesc(model.getDesc());
+            }
+        } ;
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
+    }
+
+
+    public static class BlogViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+        public BlogViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public  void setTitle(String title){
+            TextView posttitle= (TextView) mView.findViewById(R.id.posttitle);
+            posttitle.setText(title);
+        }
+
+        public  void setDesc(String title){
+            TextView postdesc= (TextView) mView.findViewById(R.id.posttext);
+            postdesc.setText(title);
+        }
+
+    }
 }
