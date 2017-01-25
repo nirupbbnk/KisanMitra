3720e5c7e07d08798d88ad5d68b5c7af53cba268
Niranjan P B <niranjanpb03@gmail.com>
2017-01-25 09:40:41 +0530

    Google signIn and signout

--- app/src/main/java/pathrer/com/kisanmitra/SelectionActivity.java
+++ app/src/main/java/pathrer/com/kisanmitra/SelectionActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,8 +15,8 @@ public class SelectionActivity extends AppCompatActivity {
 
     protected Button ferti;
     private Button logout;
-    private FirebaseAuth mauth;
-    private FirebaseAuth.AuthStateListener mauthlistner;
+    public FirebaseAuth mauth;
+    public FirebaseAuth.AuthStateListener mauthlistner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,5 +53,22 @@ public class SelectionActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         mauth.addAuthStateListener(mauthlistner);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()== R.id.logout){
+            mauth.signOut();
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
     }
 }
