b0291ecae7d0af339fe2b7a1a482ef43cc26f1c4
Niranjan P B <niranjanpb03@gmail.com>
2017-01-25 10:27:39 +0530

    Navigaation Drawer

--- app/src/main/java/pathrer/com/kisanmitra/SelectionActivity.java
+++ app/src/main/java/pathrer/com/kisanmitra/SelectionActivity.java
@@ -2,6 +2,8 @@ package pathrer.com.kisanmitra;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -17,10 +19,18 @@ public class SelectionActivity extends AppCompatActivity {
     private Button logout;
     public FirebaseAuth mauth;
     public FirebaseAuth.AuthStateListener mauthlistner;
+    private DrawerLayout mdrawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+
+        mdrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,mdrawerLayout,R.string.open,R.string.close);
+        mdrawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         ferti = (Button) findViewById(R.id.fertilizer);
         ferti.setOnClickListener(new View.OnClickListener() {
@@ -66,7 +76,9 @@ public class SelectionActivity extends AppCompatActivity {
         if(item.getItemId()== R.id.logout){
             mauth.signOut();
         }
-
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
         return super.onOptionsItemSelected(item);
 
 
